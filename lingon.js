961a8cdec6d010e600c706f61438505c78d8744a
Gabriel Lundberg <gabriel@spotify.com>
2015-04-26 15:13:49 +0800

    Do more initital setup
    Add auto-reload, and auto-generate CSS vendor prefixes.

--- lingon.js
+++ lingon.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
 var lingon = require('lingon');
+var lr = require('lingon-livereload');
+var autoprefixer = require('gulp-autoprefixer')
+
+// Set up auto-reload on save
+lr(lingon);
+
+// Automatically add vendor prefixes for cutting-edge CSS
+lingon.postProcessors.push('less', function() {
+  return autoprefixer();
+});
