45c74343aaae405c52ffbcb77d565aeaecb4fb50
Gabriel Lundberg <git@leirb.ag>
2016-04-02 14:38:32 +0800

    Update output with translation when selection changes

---
+++ TODO.md
@@ -0,0 +1,4 @@
+* Find monospace font (Bonus: support international phonetic alphabet characters)
+* Find or write railway station display animation
+X Convert selected boxes to string
+X Display new string
--- source/all.js
+++ source/all.js
@@ -1,1 +1,45 @@
-console.log('js!');
+var TRANSLATIONS = {
+	"coffee": "kopi",
+	"tea": "teh",
+	"evaporated": "C",
+	"nomilk": "O",
+	"nosugar": "kosong",
+	"iced": "peng"
+};
+
+function makeTranslation(tokens) {
+	var output = "";
+
+	for (var i = 0; i < tokens.length; i++) {
+		var token = tokens[i];
+
+		if (token in TRANSLATIONS) {
+			if (output != "") {
+				output = output + " ";
+			}
+			output = output + TRANSLATIONS[token];
+		}
+	}
+
+	return output;
+}
+
+function updateOutput() {
+	var checked = document.querySelectorAll("input:checked");
+
+	tokens = [];
+	for (var i = 0; i < checked.length; i++) {
+		tokens.push(checked[i].value);
+	}
+
+	document.getElementById("output").innerText = makeTranslation(tokens);
+}
+
+var checkboxes = document.querySelectorAll("input[type=radio]");
+for (var i = 0; i < checkboxes.length; i++) {
+	var element = checkboxes[i];
+
+	element.onchange = updateOutput;
+}
+
+updateOutput();
